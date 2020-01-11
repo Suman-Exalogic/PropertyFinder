d7308631b1dce615a21ae300215b2385bbec8007
Suman-Exalogic <suman@exalogic.in>
2020-01-11 15:43:39 +0530

    iccccccccc

---
+++ db/migrate/20170111101223_create_inquiries.rb
@@ -0,0 +1,12 @@
+class CreateInquiries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :inquiries do |t|
+      t.string :subject
+      t.text :message
+      t.integer :house_hunter_id
+      t.integer :house_id
+
+      t.timestamps
+    end
+  end
+end
